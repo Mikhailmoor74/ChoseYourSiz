85afb273ce169cd13223b29587562c1f51d5ff3a
Mikhailmoor74 <mikhail.moor@bk.ru>
2020-11-02 21:27:20 +0500

    Продолжаем дизайнить

---
+++ app/src/main/java/moor/foundation/choseyoursiz/SplashScreen.java
@@ -0,0 +1,34 @@
+package moor.foundation.choseyoursiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(
+                            SplashScreen.this,
+                            MainActivity.class));
+                }
+            }
+        };
+
+        thread.start();
+    }
+}
